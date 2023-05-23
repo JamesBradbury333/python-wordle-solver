0582f459166e753ba76c1a0d73a2fb92bcb88348
jamesbradbury333 <jamesbradbury222@gmail.com>
2023-05-23 18:05:30 +0100

    Add pipenv/pytest config

--- tests/unit/test_word_functions.py
+++ tests/unit/test_word_functions.py
@@ -1,3 +1,6 @@
-import pytest
+from src.main import remove_words_if_in_char_list
 
 
+def test_remove_words_if_in_char_list():
+    jedis = ["yoda", "obi-wan", "anakin", "luke"]
+    assert remove_words_if_in_char_list(jedis, ["a"]) == ["luke"]
