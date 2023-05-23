5dbb4b993f1845d5d511cbc7411ff2b9d8d9139c
jamesbradbury333 <jamesbradbury222@gmail.com>
2023-05-23 23:47:33 +0100

    add some alphabet functions

--- src/main.py
+++ src/main.py
@@ -4,32 +4,14 @@ import copy
 def main():
 
     inital_guess = "adieu"
-    with open("../5-letter-words.txt") as file:
+    with open("5-letter-words.txt") as file:
         possible_words = file.read().split("\n")
 
-    print(len(possible_words))
-
-    # for word in possible_words:
-    #     for char in inital_guess:
-    #         if char not in word:
-    #             possible_words.remove(word)
-
-    # print(len(possible_words))
-    test_list = ["adieu", "cat", "dog", "zzz"]
-    copy_list = copy.deepcopy(possible_words)
-
-    for word in possible_words:
-        for char in inital_guess:
-            if char in list(word):
-                copy_list.remove(word)
-                break
-    print(len(copy_list))
+    reduced_words = remove_words_if_in_char_list(possible_words, list(inital_guess))
+    most_common_letters = rank_most_common_letters_in_word_list(reduced_words)
+    
 
     return
-
-
-if __name__ == "__main__":
-    main()
 
 
 def remove_words_if_in_char_list(word_list, char_list):
@@ -40,5 +22,21 @@ def remove_words_if_in_char_list(word_list, char_list):
             if char in list(word):
                 copy_word_list.remove(word)
                 break
-
     return copy_word_list
+
+
+def rank_most_common_letters_in_word_list(word_list):
+    letters_dict = dict.fromkeys(alphabet_letters_list(), 0)
+    for word in word_list:
+        for letter in list(word):
+            letters_dict[letter] += 1
+
+    return letters_dict
+
+
+def alphabet_letters_list():
+    return list(map(chr, range(97, 123)))
+
+
+if __name__ == "__main__":
+    main()
--- tests/unit/test_word_functions.py
+++ tests/unit/test_word_functions.py
@@ -1,6 +1,71 @@
 from src.main import remove_words_if_in_char_list
+from src.main import alphabet_letters_list
+from src.main import rank_most_common_letters_in_word_list
 
 
 def test_remove_words_if_in_char_list():
     jedis = ["yoda", "obi-wan", "anakin", "luke"]
     assert remove_words_if_in_char_list(jedis, ["a"]) == ["luke"]
+
+
+def test_rank_most_common_letters_in_word_list():
+    word_list = ["aaa", "bb", "c"]
+    assert rank_most_common_letters_in_word_list(word_list) == {
+        "a": 3,
+        "b": 2,
+        "c": 1,
+        "d": 0,
+        "e": 0,
+        "f": 0,
+        "g": 0,
+        "h": 0,
+        "i": 0,
+        "j": 0,
+        "k": 0,
+        "l": 0,
+        "m": 0,
+        "n": 0,
+        "o": 0,
+        "p": 0,
+        "q": 0,
+        "r": 0,
+        "s": 0,
+        "t": 0,
+        "u": 0,
+        "v": 0,
+        "w": 0,
+        "x": 0,
+        "y": 0,
+        "z": 0,
+    }
+
+
+def test_alphabet_letters_list():
+    assert alphabet_letters_list() == [
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f",
+        "g",
+        "h",
+        "i",
+        "j",
+        "k",
+        "l",
+        "m",
+        "n",
+        "o",
+        "p",
+        "q",
+        "r",
+        "s",
+        "t",
+        "u",
+        "v",
+        "w",
+        "x",
+        "y",
+        "z",
+    ]
