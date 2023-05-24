063b0a57e023cd6d85d1ea09f5044f1588fbe10b
jamesbradbury333 <jamesbradbury222@gmail.com>
2023-05-24 23:55:46 +0100

    Add a couple of classes to begin tracking guessed letters

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
 import copy
+from collections import namedtuple
 
 
 def main():
 
     inital_guess = "adieu"
+    possible_letters = alphabet_letters_list()
+
+
     with open("5-letter-words.txt") as file:
         possible_words = file.read().split("\n")
 
@@ -37,6 +41,43 @@ def rank_most_common_letters_in_word_list(word_list):
 def alphabet_letters_list():
     return list(map(chr, range(97, 123)))
 
+
+class WordleWord:
+    
+    def __init__(self):
+        self.position_0 = self.alphabet_letters_dict()
+        self.position_1 = self.alphabet_letters_dict()
+        self.position_2 = self.alphabet_letters_dict()
+        self.position_3 = self.alphabet_letters_dict()
+        self.position_4 = self.alphabet_letters_dict()
+
+        self.letters_in_word = []
+
+    def alphabet_letters_list(self):
+        return list(map(chr, range(97, 123)))
+    
+    def alphabet_letters_dict(self):
+        return dict.fromkeys(self.alphabet_letters_list(), "maybe")
+    
+    def update_positions(self, LetterGuess):
+        if LetterGuess.position == 0:
+            if LetterGuess.correct_position:
+                self.position_0[LetterGuess.letter] = True
+            else:
+                self.position_0[LetterGuess.letter] = False
+                if LetterGuess.in_word:
+                    self.letters_in_word.append(LetterGuess.letter)
+
+
+class LetterGuess:
+    def __init__(self, letter: str, position: int ,correct_position: bool, in_word: bool):
+        self.letter = letter
+        self.positon = position
+        self.correct_posiiton = correct_position
+        self.in_word = in_word
+
+
+        
 
 if __name__ == "__main__":
     main()
